62443bb6b7f94a8cf16bd10ba8b10c6b11a318a2
pradeepmali269 <pradeepmali269@gmail.com>
2026-02-25 19:07:16 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Body from "./components/Body";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile 
+            />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
